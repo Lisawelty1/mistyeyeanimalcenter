f0c61d0479a8b632fb2ccce4ebca63bb9bc76f79
Lisawelty1 <lisawelty46@gmail.com>
2017-07-04 23:26:19 -0400

    1

---
+++ theme-runcharity/process.php
@@ -0,0 +1,19 @@
+<?php
+
+    $to = "lisawelty46@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name']; 
+    $headers = "From: $from";
+    $subject = "Misty Eyes Animal Center";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"phone"} = "phone";
+    $fields{"message"} = "message";
+
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
